58a3f9b52d816b9e6f992668944777c790703100
Julian Shen <julianshen@gmail.com>
2021-12-14 22:33:33 +0800

    Update latestver.sh

--- latestver.sh
+++ latestver.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+jq --help
 curl --silent "https://api.github.com/repos/helium/blockchain-etl/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
     sed -E 's/.*"([^"]+)".*/\1/'
