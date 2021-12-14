9b1b168fe71bc35dfe115e9f9fda3cea11aac579
Julian Shen <julianshen@gmail.com>
2021-12-14 22:37:44 +0800

    Update download_latest.sh

--- download_latest.sh
+++ download_latest.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-jq --help
-release_tag=$(curl --silent "https://api.github.com/repos/helium/blockchain-etl/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/')
+release_tag=$(curl --silent "https://api.github.com/repos/helium/blockchain-etl/tags" | jq -r '.[0].name')
 
 git clone -b $release_tag -q https://github.com/helium/blockchain-etl /usr/src/etl
  
